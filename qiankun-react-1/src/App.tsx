c02d68ca7ee504f149f124111fdb78ec23ef9401
niuziyang <niuziyang@tezign.com>
2021-07-01 18:29:13 +0800

    feat: add react vue app

---
+++ qiankun-react-1/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <>你好啊，这里是react</>;
+}
+
+export default App;
---
+++ qiankun-react-1/src/index.js
@@ -0,0 +1,41 @@
+import "./public-path";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+function render(props) {
+  const { container } = props;
+  ReactDOM.render(
+    <App />,
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log("[react16] react app bootstraped");
+}
+
+export async function mount(props) {
+  console.log("[react16] props from main framework", props);
+  props.onGlobalStateChange((state, prev) => {
+    // state: 变更后的状态; prev 变更前的状态
+    console.log(state, prev);
+  });
+  props.setGlobalState({ a: 1 });
+  render(props);
+}
+
+export async function unmount(props) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
